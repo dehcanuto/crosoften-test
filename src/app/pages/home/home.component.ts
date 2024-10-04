54bdb0b54e3b6fe69dfaa3485a7330988e9ef6b5
André Canuto <andrecanutoc@gmail.com>
2024-10-04 12:44:09 -0300

    feat: list countries

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { finalize, Subject, take } from 'rxjs';
+
+import { CountryType } from '../../models/country.model';
+import { CountryRegionsType } from '../../models/regions.model';
+import { CountriesService } from '../../services/countries.service';
+import { RegionEnum } from '../../enums/region.enum';
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  public  loading: Boolean = true;
+  public  countries!: CountryType[];
+  public  regions: CountryRegionsType[] = [];
+  private destroyed$ = new Subject<void>();
+  
+  constructor(private countriesService: CountriesService) {}
 
+  ngOnInit(): void {
+    this.getAllCountries().subscribe(countries => this.countries = countries);
+
+    this.regions = Object.values(RegionEnum).map(value => ({
+      value,
+      label: value[0].toUpperCase() + value.slice(1),
+    }));
+  }
+
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+    this.destroyed$.complete();
+  }
+
+  private getAllCountries() {
+    return this.countriesService.getAllCountries().pipe(
+      finalize(() => {
+        this.loading = false;
+      }),
+      take(1),
+    );
+  }
 }
