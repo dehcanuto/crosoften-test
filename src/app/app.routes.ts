4a6c65f1b756b0f1e7270c08585e9ed226af4f43
André Canuto <andrecanutoc@gmail.com>
2024-10-03 23:16:44 -0300

    feat: pages routes init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailsComponent } from './pages/details/details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'country', children: [
+     { path: ':id', component: DetailsComponent }
+    ]}
+];
